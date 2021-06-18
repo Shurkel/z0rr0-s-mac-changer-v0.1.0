167b90cc066870a7e81eaaf29a7db1a575494ce3
z0rr0 <70747657+z3rr0z0rr0@users.noreply.github.com>
2021-06-18 18:08:26 +0100

    Update z0rr0's_mac_changer_terminal_ui_v0.1.0.py

--- z0rr0's_mac_changer_terminal_ui_v0.1.0.py
+++ z0rr0's_mac_changer_terminal_ui_v0.1.0.py
@@ -26,7 +26,7 @@ new_mac = input("[$] Please specify a new MAC address >> ")
 print("[...] Changing MAC address for " + interface + " to " + new_mac)
 
 subprocess.call(["ifconfig", interface, "down"])
-subprocess.call(["ifconfig", interface, "hw ether", new_mac])
+subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
 subprocess.call(["ifconfig", interface, "up"])
 subprocess.call(["ifconfig"])
 
